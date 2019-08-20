9563999122a7c19c6f868919351ebf0a1c0a43e3
lvshuy <lsy-181@qq.com>
2019-08-20 22:25:09 +0800

    commit

---
+++ config02/src/main/java/com/springboot/Config02Application.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Config02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Config02Application.class, args);
+	}
+
+}
